d7062893417ce11520f7a3a9e829c546e9718010
Mellany Linhares <mellinhares6@gmail.com>
2025-05-09 17:13:56 -0300

    feat: add schemas

--- app/core/constants.py
+++ app/core/constants.py
@@ -4,3 +4,9 @@ from enum import Enum
 class Environment(str, Enum):
     LOCAL = "local"
     TEST = "test"
+
+
+class EnrollmentStatus(int, Enum):
+    IN_QUEUE = 1
+    ACCEPTED = 2
+    REFUSED = 3
--- app/fields.py
+++ app/fields.py
@@ -1,5 +1,11 @@
 from typing import Annotated
 
-from pydantic import BeforeValidator
+from pydantic import BeforeValidator, AfterValidator, Field
 
-PyObjectId = Annotated[str, BeforeValidator(str)]
+from app.utils import validate_cpf
+
+PyObjectId = Annotated[str, Field(alias="_id"), BeforeValidator(str)]
+
+Age = Annotated[int, Field(ge=0, lt=150)]
+
+Cpf = Annotated[str, Field(pattern=r"^\d{3}\.\d{3}\.\d{3}\-\d{2}$"), AfterValidator(validate_cpf)]
