06894a4f150eb21cbc808b97fcb6d8a20d2c3bb1
Mellany Linhares <mellinhares6@gmail.com>
2025-05-09 12:23:47 -0300

    chore: update project build

--- app/core/app.py
+++ app/core/app.py
@@ -1,22 +1,22 @@
 from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
+from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse, Response
 
 from app.core.settings import Settings
 from app.routers import router
 
 
-async def invalid_data(_: Request, exc: Exception) -> Response:
-    return JSONResponse(status_code=422, content={"error": str(exc)})
-
-
 async def internal_error(_: Request, exc: Exception) -> Response:
     return JSONResponse(status_code=500, content={"error": str(exc)})
 
 
+async def http_exception_handler(_: Request, exc: HTTPException) -> Response:
+    return JSONResponse(status_code=exc.status_code, content={"error": exc.detail})
+
+
 def init_app(settings: Settings) -> FastAPI:
     exception_handlers = {
-        RequestValidationError: invalid_data,
+        HTTPException: http_exception_handler,
         Exception: internal_error,
     }
 
